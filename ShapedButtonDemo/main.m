9e80fe79eeb8449d84a3b0156fdbca4889521d15
ole <ole@oleb.net>
2009-10-17 17:05:43 +0200

    Created new demo project

---
+++ ShapedButtonDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ShapedButtonDemo
+//
+//  Created by Ole Begemann on 17.10.09.
+//  Copyright Ole Begemann 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
