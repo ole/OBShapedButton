d15cac0cb050dd30beb238b5ebc2309160e7f547
ole <ole@oleb.net>
2009-10-17 16:18:41 +0200

    Finished renaming of files. Added license and documentation to OBShapedButton.

--- Classes/OBShapedButton.h
+++ Classes/OBShapedButton.h
@@ -1,16 +1,60 @@
-//
-//  OBNonRectButton.h
-//  NonRectangularButton
-//
-//  Created by Ole Begemann on 16.10.09.
-//  Copyright 2009 Ole Begemann. All rights reserved.
-//
+/*
+ Copyright (c) 2009 Ole Begemann
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
+/*
+ OBShapedButton.h
+ 
+ Created by Ole Begemann
+ October, 2009
+ */
 
 #import <UIKit/UIKit.h>
 
 
-@interface OBNonRectButton : UIButton {
+/*
+ OBShapedButton is a UIButton subclass optimized for non-rectangular button shapes.
+ Instances of OBShapedButton respond to touches only in areas where the image that is
+ assigned to the button for UIControlStateNormal is non-transparent.
+ 
+ Usage:
+ - Add OBShapedButton.h, OBShapedButton.m, UIImage+ColorAtPixel.h, and UIImage+ColorAtPixel.m
+   to your Xcode project.
+ - Design your UI in Interface Builder with UIButtons as usual. Set the Button type to Custom
+   and provide transparent PNG images for the different control states as needed.
+ - In the Identity Inspector in Interface Builder, set the Class of the button to OBShapedButton.
+   That's it! Your button will now only respond to touches where the PNG image for the normal
+   control state is non-transparent.
+ */
 
+
+
+// -[UIView hitTest:withEvent: ignores views that an alpha level less than 0.1.
+// So we will do the same and treat pixels with alpha < 0.1 as transparent.
+#define kAlphaVisibleThreshold (0.1f);
+
+
+@interface OBShapedButton : UIButton {
+    // Our class interface is empty. OBShapedButton only overwrites one method of UIView.
+    // It has no attributes of its own.
 }
 
 @end
--- Classes/OBShapedButton.m
+++ Classes/OBShapedButton.m
@@ -1,22 +1,38 @@
-//
-//  OBNonRectButton.m
-//  NonRectangularButton
-//
-//  Created by Ole Begemann on 16.10.09.
-//  Copyright 2009 Ole Begemann. All rights reserved.
-//
+/*
+ Copyright (c) 2009 Ole Begemann
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
 
-#import "OBNonRectButton.h"
+/*
+ OBShapedButton.m
+ 
+ Created by Ole Begemann
+ October, 2009
+ */
+
+#import "OBShapedButton.h"
 #import "UIImage+ColorAtPixel.h"
 
 
-// -[UIView hitTest:withEvent: ignores views that an alpha level less than 0.1.
-// So we will do the same and treat pixels with alpha < 0.1 as transparent.
-#define kAlphaVisibleThreshold (0.1f);
 
-
-
-@implementation OBNonRectButton
+@implementation OBShapedButton
 
 // UIView uses this method in hitTest:withEvent: to determine which subview should receive a touch event.
 // If pointInside:withEvent: returns YES, then the subview’s hierarchy is traversed; otherwise, its branch
