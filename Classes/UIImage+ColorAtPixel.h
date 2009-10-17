14c1b1f45372658dccbc441e1bf1031a3a536771
ole <ole@oleb.net>
2009-10-17 15:34:56 +0200

    Added MIT license to UIImage+ColorAtPixel. Addedcomments to UIImage+ColorAtPixel. colorAtPixel: now uses the Generic RGB color space.

--- Classes/UIImage+ColorAtPixel.h
+++ Classes/UIImage+ColorAtPixel.h
@@ -1,14 +1,38 @@
-//
-//  UIImage+ColorAtPixel.h
-//  NonRectangularButton
-//
-//  Created by Ole Begemann on 16.10.09.
-//  Copyright 2009 Ole Begemann. All rights reserved.
-//
+/*
+ Copyright (c) 2009 Ole Begemann
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
 
+/*
+ UIImage+ColorAtPixel.h
+
+ Created by Ole Begemann
+ October, 2009
+ */
+ 
 #import <UIKit/UIKit.h>
 
-
+/*
+ A category on UIImage that enables you to query the color value of arbitrary 
+ pixels of the image.
+ */
 @interface UIImage (ColorAtPixel)
 
 - (UIColor *)colorAtPixel:(CGPoint)point;
--- Classes/UIImage+ColorAtPixel.m
+++ Classes/UIImage+ColorAtPixel.m
@@ -1,31 +1,64 @@
-//
-//  UIImage+ColorAtPixel.m
-//  NonRectangularButton
-//
-//  Created by Ole Begemann on 16.10.09.
-//  Copyright 2009 Ole Begemann. All rights reserved.
-//
+/*
+ Copyright (c) 2009 Ole Begemann
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
+/*
+ UIImage+ColorAtPixel.m
+ 
+ Created by Ole Begemann
+ October, 2009
+ */
 
 #import "UIImage+ColorAtPixel.h"
 
 
 @implementation UIImage (ColorAtPixel)
 
+/*
+ Returns the color of the image pixel at point in the Generic RGB color space.
+ Returns nil if point lies outside the image bounds.
+ If the point coordinates contain decimal parts, they will be truncated.
+ 
+ To get at the pixel data, this method must draw the image into a bitmap context.
+ For minimal memory usage and optimum performance, only the specific requested
+ pixel is drawn.
+ If you need to query pixel colors for the same image repeatedly (e.g., in a loop),
+ this approach is probably less efficient than drawing the entire image into memory
+ once and caching it.
+ */
 - (UIColor *)colorAtPixel:(CGPoint)point {
-    
     // Cancel if point is outside image coordinates
     if (!CGRectContainsPoint(CGRectMake(0.0f, 0.0f, self.size.width, self.size.height), point)) {
         return nil;
     }
     
-    // Draw just the pixel we are interested in into a 1x1-pixel byte array
+    
+    // Create a 1x1 pixel byte array and bitmap context to draw the pixel into.
     // Reference: http://stackoverflow.com/questions/1042830/retrieving-a-pixel-alpha-value-for-a-uiimage
     NSInteger pointX = trunc(point.x);
     NSInteger pointY = trunc(point.y);
     CGImageRef cgImage = self.CGImage;
     NSUInteger width = CGImageGetWidth(cgImage);
     NSUInteger height = CGImageGetHeight(cgImage);
-    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateWithName(kCGColorSpaceGenericRGB);
     int bytesPerPixel = 4;
     int bytesPerRow = bytesPerPixel * 1;
     NSUInteger bitsPerComponent = 8;
@@ -38,8 +71,10 @@
                                                  colorSpace,
                                                  kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
     CGColorSpaceRelease(colorSpace);
-    CGContextTranslateCTM(context, -pointX, -pointY);
     CGContextSetBlendMode(context, kCGBlendModeCopy);
+
+    // Draw the pixel we are interested in onto the bitmap context
+    CGContextTranslateCTM(context, -pointX, -pointY);
     CGContextDrawImage(context, CGRectMake(0.0f, 0.0f, (CGFloat)width, (CGFloat)height), cgImage);
     CGContextRelease(context);
     
